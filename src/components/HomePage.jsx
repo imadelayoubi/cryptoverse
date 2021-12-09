a3969013f0153948fc41ec01308a53d322bc3537
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-12-09 16:52:38 +0100

    working on hompage component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import millify from 'millify';
+import { Typography,Row,Col,Statistic } from "antd";
+
+const HomePage = () => {
+    return (
+    <div>
+        Homepage
+        </div>
+   )
+}
+
+
+export default HomePage;
